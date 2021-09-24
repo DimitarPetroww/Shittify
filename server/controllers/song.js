d33420ffa66a438ee80d0b75f359ca3394166b22
Dimitar.Petroww <Mitko66692@abv.bg>
2021-09-25 00:47:49 +0300

    update

--- server/controllers/song.js
+++ server/controllers/song.js
@@ -9,10 +9,13 @@ router.post("/create", async (req, res) => {
     const form = formidable()
     try {
         const [fields, files] = await parseForm(req, form)
+        if (Object.values(fields).some(x => x === "") || Object.keys(files).length === 0) {
+            throw new Error("All fields are required")
+        }
         const [image, imageId] = await cloudinaryUpload(files.image.path)
         const [audio, audioId] = await cloudinaryUpload(files.audio.path, "video")
-        const song = await songService.createSong({...fields, image, imageId, audio, audioId, owner: req.user._id})
-        
+        const song = await songService.createSong({ ...fields, image, imageId, audio, audioId, owner: req.user._id })
+
         res.json(song)
     } catch (e) {
         res.status(400)
